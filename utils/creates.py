2b1855e24a250f385a6f5ebabebd2c6d9bbcae4b
Sharon <sharonlin14@gmail.com>
2016-11-05 15:44:56 -0400

    Added check_Story

--- utils/creates.py
+++ utils/creates.py
@@ -1,4 +1,5 @@
-import sqlite3, datetime
+from itertools import *
+import sqlite3, datetime, itertools
 
 #Create a completely new story
 
@@ -14,17 +15,18 @@ def create_story(u_username, u_title, u_recent):
 
     c.execute("INSERT INTO stori (story_title, most_recent, number_edits, time_since) VALUES (?, ?, 1, 0)", params)
     c.execute("INSERT INTO full_stori (story_title, full_text) VALUES (?,?)", params)
-    curr_id = str(c.execute("SELECT story_id FROM stori WHERE story_title = ?", (u_title,)))
-    c.execute("UPDATE useri SET story_id = story_id || ? || ','  WHERE username= ?", (curr_id, u_username))
+    c.execute("SELECT story_id FROM stori WHERE story_title = ?", (u_title,))
+    list_of_ids = list(itertools.chain.from_iterable(c))
+    c.execute("UPDATE useri SET story_id = story_id || ? || ','  WHERE username= ?", (list_of_ids[0], u_username))
 # Note: store timestamp as TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-# UIUD replaces normal id as a "data type"    
+# Note: initiate users story_id with "," for this to work
 
     db.commit()
     db.close()
 
 # Deletes an existing story
 
-#create_story("sammi", "mystory", "many things")
+#create_story("sammi", "asdfasdf", "amany things")
 
 def delete(u_title):
     #==============================================
@@ -49,17 +51,18 @@ def add_story(u_username, u_title, u_recent):
     db = sqlite3.connect(f)
     c = db.cursor()
 
-#    if (c.execute("SELECT number_edits FROM stori WHERE story_title = ?", (u_title,))<=15):
-    c.execute("UPDATE stori SET most_recent = ?, number_edits = number_edits+1, time_since = 0, timestamp = CURRENT_TIMESTAMP WHERE story_title = ?", (u_recent, u_title))
+    c.execute("SELECT number_edits FROM stori WHERE story_title = ?", (u_title,))
+    list_of_edits = list(itertools.chain.from_iterable(c))
 
-    c.execute("UPDATE full_stori SET full_text = full_text||' '||? WHERE story_title = ?", (u_recent, u_title))
+    if (list_of_edits[0]<16):
+        c.execute("UPDATE stori SET most_recent = ?, number_edits = number_edits+1, time_since = 0, timestamp = CURRENT_TIMESTAMP WHERE story_title = ?", (u_recent, u_title))
+
+        c.execute("UPDATE full_stori SET full_text = full_text||' '||? WHERE story_title = ?", (u_recent, u_title))
 
     db.commit()
     db.close()
 
 # Checks if a user has edited yet
-
-add_story("sammi", "mystory", "There is antoher thing!")
 
 def check_story(u_username, u_id):
     #==============================================
@@ -67,15 +70,19 @@ def check_story(u_username, u_id):
     db = sqlite3.connect(f)
     c = db.cursor()
 
-    storys = c.execute("SELECT story_ids FROM users WHERE user_id = ?", u_id)
-    mylist = map(storys.split(',') )
-    for x in mylist:
-        if x == u_id:
-            db.commit()
-            db.close()
-            return True
+    c.execute("SELECT story_id FROM useri WHERE username = ?", (u_username,))
+    list_of_ids = list(itertools.chain.from_iterable(c))
+    listH = list_of_ids[0].split(',')
+    for x in listH:
+        print(x)
+        if x != '':
+            if int(x) == int(u_id):
+                db.commit()
+                db.close()
+                return True
 
     db.commit()
     db.close()
     return False 
 
+#print(check_story("sammi", 18))
